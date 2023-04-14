919f2e0f01aa86e2c574114958e870388eb2ebf1
David Gabriel <davidg2642@gmail.com>
2023-04-14 16:28:45 -0300

    clase 02

---
+++ Clase02/src/main/java/ap40/clase02/Clase02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ap40.clase02;
+
+/**
+ *
+ * @author david
+ */
+public class Clase02 {
+
+    public static void main(String[] args) {
+        System.out.println("git y github");
+    }
+}
